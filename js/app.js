a4cc99b2c4e0b9cbc66b93f6b1e3e9d80aa22753
izzat <izzatabs@gmail.com>
2023-10-24 17:37:56 +0500

    added services section width swiper js

--- js/app.js
+++ js/app.js
@@ -3,11 +3,33 @@ let bannerItems = document.querySelectorAll(".banner-content_item");
 
 console.log(bannerItems);
 let zero = 0;
-window.addEventListener("scroll", () => {
-    if (scrollY > banner.offsetTop - banner.clientHeight / 3) {
-        bannerItems.forEach((item) => {
-            item.style = `transform:translateX(${zero});
-                                  opacity:${1};`;
-        });
-    }
+
+// window.addEventListener("scroll", () => {
+//     if (scrollY > banner.offsetTop - banner.clientHeight / 3) {
+//         bannerItems.forEach((item) => {
+//             item.style = `transform:translateX(${zero});
+//                                   opacity:${1};`;
+//         });
+//     }
+// });
+
+// Swiper js code
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 37,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+// Animations
+
+window.addEventListener("DOMContentLoaded", () => {
+    bannerItems.forEach((el) => {
+        el.style = `transform:translateX(${zero});
+                            opacity:${1};`;
+    });
 });
