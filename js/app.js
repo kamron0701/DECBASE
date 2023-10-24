ed792d295d50e3063d6e36227f8402774b08ea64
izzat <izzatabs@gmail.com>
2023-10-24 18:04:08 +0500

    added about us section

--- js/app.js
+++ js/app.js
@@ -1,24 +1,28 @@
 let banner = document.querySelector(".banner");
 let bannerItems = document.querySelectorAll(".banner-content_item");
+let aboutUsItems = document.querySelectorAll(".about-us_item");
+let about = document.querySelector(".about");
 
 console.log(bannerItems);
 let zero = 0;
 
-// window.addEventListener("scroll", () => {
-//     if (scrollY > banner.offsetTop - banner.clientHeight / 3) {
-//         bannerItems.forEach((item) => {
-//             item.style = `transform:translateX(${zero});
-//                                   opacity:${1};`;
-//         });
-//     }
-// });
+console.log(aboutUsItems);
+
+window.addEventListener("scroll", () => {
+    if (scrollY > about.offsetTop - about.clientHeight / 2) {
+        aboutUsItems.forEach((item) => {
+            item.style = `transform:translateX(${zero});
+                                  opacity:${1};`;
+        });
+    }
+});
 
 // Swiper js code
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 37,
-
+    autoplay: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
