80d4c01c5976cbbece6c2a3877faf3b0fdacdc7d
izzat <izzatabs@gmail.com>
2023-10-31 18:00:57 +0500

    added animations

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
+// Banner section
+
 let banner = document.querySelector(".banner");
 let bannerItems = document.querySelectorAll(".banner-content_item");
+
+// Service section
+let servicesSection = document.querySelector(".services");
+let serviceSubtitle = document.querySelector(".services-subtitle");
+let serviceTitle = document.querySelector(".services-title");
+
+console.log(serviceSubtitle);
+
+// About us section
 let aboutUsItems = document.querySelectorAll(".about-us_item");
 let about = document.querySelector(".about");
 
+// Experience section
 let experience = document.querySelector(".experience");
 let experienceItems = document.querySelectorAll(".experience-content_item");
 
-console.log(experience);
-console.log(experienceItems);
+// Testimonials
+let testimonials = document.querySelector(".testimonials");
+let testTitle = document.querySelector(".testimonial-title");
+let testSubtitle = document.querySelector(".testimonial-subtitle");
 
 let zero = 0;
 
@@ -37,6 +51,24 @@ window.addEventListener("scroll", () => {
             item.style = `transform:translateX(${zero});
                                   opacity:${1};`;
         });
+    }
+});
+
+window.addEventListener("scroll", () => {
+    if (
+        scrollY >
+        servicesSection.offsetTop - servicesSection.clientHeight / 2
+    ) {
+        serviceSubtitle.style.transform = `translateY(0)`;
+        serviceSubtitle.style.opacity = 1;
+        serviceTitle.style.transform = `translateY(0)`;
+        serviceTitle.style.opacity = 1;
+    }
+});
+window.addEventListener("scroll", () => {
+    if (scrollY > testimonials.offsetTop - testimonials.clientHeight / 2) {
+        testTitle.style.transform = `rotateY(720deg)`;
+        testTitle.style.opacity = 1;
     }
 });
 
